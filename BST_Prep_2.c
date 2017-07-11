b79b1f6f5a09082d4e655bed3464609937e07bbf
AbdulTheProgrammer <abdulahhadi@hotmail.com>
2017-07-10 22:44:37 -0400

    updated BST_Prep_2.c

--- BST_Prep_2.c
+++ BST_Prep_2.c
@@ -50,3 +50,96 @@ void inorder(node* root){
     visit(root);
     inorder(root->right);
 }
+
+
+/*
+ 
+ Harder Question:
+ 
+ find the next node (inorder successor) of a given node in a BST. you may assume that each node has a link to its parent
+ 
+ i.e. node->parent
+ */
+node* Node {
+    node* left;
+    node* right;
+    node* parent;
+}
+
+void inorder(node* root){
+    if(root==NULL)
+        return;
+    
+    inorder(root->left);
+    visit(root);
+    inorder(root->right);
+}
+
+node* find_successor(node* root){
+    
+    //first
+    if (root->right == NULL){
+        
+        while (root->parent->right == root){
+            root = root->parent;
+        }
+        if (root->parent->left == root ){
+            return root->parent;
+        } else if (root->parent == NULL){
+            return NULL;
+        }
+    }
+    
+    //second
+    if (root->right!=NULL){
+        root = root->right;
+        while (root->left!=NULL){
+            root = root->left;
+        }
+        return root;
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+prev = n
+while(curr != NULL &&  != prev) {
+    
+}
+return parent
+
+
+//abhishek
+Node** findSuccessor(Node* n) {
+    if (n->parent == NULL)
+        return NULL;
+    //case 1: right child
+    if(n->right == NULL){
+        if (n->parent->right == n){
+            while(n->parent == NULL && n->parent->right == n){
+                n = n->parent;
+            }
+            return n;
+        }
+        
+    }
+    //case 2: left child find leftmost child from right child
+    n = n->right;
+    while(n->left != NULL){
+        n = n->left;
+    }
+    return n;
+    
+}
+
+
+
+
