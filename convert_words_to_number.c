3ee7b875bd405aacafeae352aedd218fdfba6cb3
Md Tahsin Sharif <mt2shari@uwaterloo.ca>
2017-08-08 22:30:59 -0400

    string to number conversion

--- convert_words_to_number.c
+++ convert_words_to_number.c
@@ -7,7 +7,7 @@
 //
 
 //LOOKUP TABLES
-
+// given a number like "one hundred ninety six", convert it to its corresponding number = 196
 static readonly Dictionary<string, byte> dic1 = new Dictionary<string, byte> {
     { "one", 1 }, { "two", 2 }, { "three", 3 }, { "four", 4 }, { "five", 5 },
     { "six", 6 }, { "seven", 7 }, { "eight", 8 }, { "nine", 9 },{ "ten", 10 },
